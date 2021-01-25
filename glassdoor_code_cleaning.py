e2c50bee81327f1504b81bd877e3979e13e9a5e9
Primit Patel <67973717+PrimitPatel@users.noreply.github.com>
2021-01-24 23:19:07 -0500

    Add files via upload

--- glassdoor_code_cleaning.py
+++ glassdoor_code_cleaning.py
@@ -6,61 +6,66 @@ Created on Tue Jul  7 13:57:29 2020
 """
 
 import pandas as pd 
+import time
+
 
 df = pd.read_csv('glassdoor_data.csv')
 
+# remove missing values in salary section
 df.dropna(subset=['Salary Estimate'], inplace=True)  # remove missing values in salary section
-df['Hourly_salary'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0) # distinguise salary based o per hour
+
+# distinguise salary based on per hour and employeer provided salary
+df['Hourly_salary'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0) 
 df['Employer_provided_salary'] = df['Salary Estimate'].apply \
     (lambda x: 1 if 'employer provided salary:' in x.lower() else 0)
 
 # Salary column contains  many negative numbers so remove these rows as salary should be integer
 df=df[df['Salary Estimate']!='-1']  
 
+# remove (Glassdoor est.) from salary column
 salary = df['Salary Estimate'].apply(lambda x: x.split('(')[0])
-minus_Kd = salary.apply(lambda x: x.replace('K','').replace('$',''))
 
-min_hr = minus_Kd.apply(lambda x: x.lower().replace('per hour','').replace('employer provided salary:',''))
+# remove K and $ from salary
+delete_k_ = salary.apply(lambda x : x.replace('K' ,'').replace('$' ,''))
+                            
 
-df['min_salary'] = min_hr.apply(lambda x: int(x.split('-')[0]))
-df['max_salary'] = min_hr.apply(lambda x: int(x.split('-')[1]))
+# remove pr hour and employeer provided salary from salary
+delete_hr_eps = delete_k_.apply(lambda x: x.lower().replace('per hour','').replace('employer provided salary:',''))
+
+df['min_salary'] = delete_hr_eps.apply(lambda x: int(x.split('-')[0]))
+df['max_salary'] = delete_hr_eps.apply(lambda x: int(x.split('-')[1]))
 df['avg_salary'] = (df.min_salary+df.max_salary)/2
 
-#Company name text only
+#company name should be text only
 df['company_txt'] = df.apply(lambda x: x['Company Name'] if x['Rating'] <0 else x['Company Name'][:-3], axis = 1)
 
-#state field 
-df['job_state'] = df['Location'].apply(lambda x: x.split(',')[1])
-df.job_state.value_counts()
+# get state field from locaton column
+df['state'] = df['Location'].apply(lambda x: x.split(',')[1])
+df.state.value_counts()
+# los angeles is city so get its state CA
+df['state'] = df.state.apply(lambda x: 'CA' if x.strip().lower() == 'los angeles' else x.strip())
+df.state.value_counts()
 
 df['same_state'] = df.apply(lambda x: 1 if x.Location == x.Headquarters else 0, axis = 1)
 
-#age of company 
-df['age'] = df.Founded.apply(lambda x: x if x <1 else 2021 - x)
+# get age of company fromcurrent year 
+year = int(time.strftime("%Y"))
+df['age'] = df.Founded.apply(lambda x: x if x <1 else year - x)
 
-#parsing of job description (python, etc.)
+# remove first unnamed column 
+df = df.drop(['Unnamed: 0'], axis =1)
 
-#python
-df['python_yn'] = df['Job Description'].apply(lambda x: 1 if 'python' in x.lower() else 0)
- 
-#r studio 
-df['R_yn'] = df['Job Description'].apply(lambda x: 1 if 'r studio' in x.lower() or 'r-studio' in x.lower() else 0)
-df.R_yn.value_counts()
+#parsing of job description 
+df['Python'] = df['Job Description'].apply(lambda x: 1 if 'python' in x.lower() else 0)
+df['R'] = df['Job Description'].apply(lambda x: 1 if 'r studio' in x.lower() or 'r-studio' in x.lower() else 0)
+df['SQL'] = df['Job Description'].apply(lambda x: 1 if 'sql' in x.lower() else 0)
+df['Spark'] = df['Job Description'].apply(lambda x: 1 if 'spark' in x.lower() else 0)
+df['AWS'] = df['Job Description'].apply(lambda x: 1 if 'aws' in x.lower() else 0)
+df['Tableau'] = df['Job Description'].apply(lambda x: 1 if 'tableau' in x.lower() else 0)
+df['PowerBi'] = df['Job Description'].apply(lambda x: 1 if 'power bi' in x.lower() or 'powerbi' in x.lower() or 'power-bi' in x.lower() else 0)
+df['Excel'] = df['Job Description'].apply(lambda x: 1 if 'excel' in x.lower() else 0)
 
-#spark 
-df['spark'] = df['Job Description'].apply(lambda x: 1 if 'spark' in x.lower() else 0)
-df.spark.value_counts()
-
-#aws 
-df['aws'] = df['Job Description'].apply(lambda x: 1 if 'aws' in x.lower() else 0)
-df.aws.value_counts()
-
-#excel
-df['excel'] = df['Job Description'].apply(lambda x: 1 if 'excel' in x.lower() else 0)
-df.excel.value_counts()
 
 df.columns
 
-df_out = df.drop(['Unnamed: 0'], axis =1)
-
-df_out.to_csv('salary_data_cleaned.csv',index = False)
+df.to_csv('glassdoor_data_cleaned.csv',index = False)
--- glassdoor_code_collection.py
+++ glassdoor_code_collection.py
@@ -6,12 +6,12 @@ Created on Sun Jun 28 16:47:00 2020
 """
 
 
-import glassdoor_data_scraper as gs 
+import glassdoor_code_scraper as gs 
 #import pandas as pd 
 # set path for chromedriver according
 path="D:/Career_learning/glasdoor_job_prediction/glassdor_2/chromedriver"
 
-df = gs.get_jobs('data scientist',300, False, path, 15)
+df = gs.get_jobs('data scientist',950, False, path, 15)
 
 df.to_csv('glassdoor_data.csv', index = False)
 df
